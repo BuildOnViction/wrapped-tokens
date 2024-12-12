b52e3cebb6b90af37fff8adbed9b14874c8f18ea
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2024-12-12 11:11:30 +0700

    add Wrapped MEW token

---
+++ contracts/presets/WrappedMEW.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "../VRC25Permit.sol";
+
+contract WrappedMEW is VRC25Permit {
+    using Address for address;
+    event Hello(address sender);
+
+    constructor() public VRC25("Wrapped MEW", "WMEW", 18) {
+    }
+
+    /**
+     * @notice Calculate fee required for action related to this token
+     * @param value Amount of fee
+     */
+    function _estimateFee(uint256 value) internal view override returns (uint256) {
+        return minFee();
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override returns (bool) {
+        return interfaceId == type(IVRC25).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @notice Issues `amount` tokens to the designated `address`.
+     *
+     * Can only be called by the current owner.
+     */
+    function mint(address recipient, uint256 amount) external onlyOwner returns (bool) {
+        _mint(recipient, amount);
+        return true;
+    }
+}
